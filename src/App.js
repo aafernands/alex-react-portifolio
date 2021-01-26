5cdd29fce309014099e35cf625e239383c66e1f0
Alex Fernandes <alexfernands@outlook.com>
2021-01-26 16:14:36 -0500

    reorganized files in folders

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import NavTabs from "./components/NavTabs";
-import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Contact from "./components/pages/Contact";
-import Portifolio from "./components/pages/Portifolio";
-import Footer from "./components/Footer";
+import NavTabs from "./components/NavTabs/NavTabs";
+import Footer from "./components/Footer/Footer";
 
+import Home from "./components/HomePage/Home";
+import About from "./components/AboutPage/About";
+import Contact from "./components/ContactPage/Contact";
+import Portifolio from "./components/PortifolioPage/Portifolio";
 
 function App() {
 	return (
@@ -17,13 +17,11 @@ function App() {
 				<Route exact path="/about" component={About} />
 				<Route exact path="/contact" component={Contact} />
 				<Route path="/portifolio" component={Portifolio} />
-        <Route path="/footer" component={Footer} />
+				<Route path="/footer" component={Footer} />
 			</div>
 
-      <Footer />
-
+			<Footer />
 		</Router>
-    
 	);
 }
 

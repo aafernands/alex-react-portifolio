07940955a9a5d2b6babd7fc37aa075aa34674d99
Alex Fernandes <alexfernands@outlook.com>
2021-01-25 22:52:56 -0500

    installed bootstrap and react-router-dom and created components

---
+++ components/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link, Route } from "react-router-dom";
+
+function Contact(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Contact;

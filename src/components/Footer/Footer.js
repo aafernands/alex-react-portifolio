434756453544b8d94c9b746a5ee5ac26c02cea94
Alex Fernandes <alexfernands@outlook.com>
2021-01-30 16:22:37 -0500

    update footer and fix zindex overlay

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,27 +1,30 @@
 import React from "react";
 import "./Footer.css";
 
-
 function Footer() {
-
-
 	return (
-    
-    
-      <div>
-
-<footer>
-            <h5>GET IN TOUCH</h5>
-            <a href="https://twitter.com/alexafernands/" className="fa fa-twitter"></a>
-            <a href="https://www.linkedin.com/in/alex-fernandes-5850b535/" className="fa fa-linkedin"></a>
-            <a href="https://www.instagram.com/aafernands/" className="fa fa-instagram"></a>
-            <a href="https://github.com/aafernands/" className="fa fa-github"></a>
-            <p><span className="credits-footer">© 2021 Website Designed by <a
-                        href="https://twitter.com/alexafernands">AlexFernandes</a></span></p>
-        </footer>  
-
-        </div>
-
+		<footer>
+			<h5>GET IN TOUCH</h5>
+			<a
+				href="https://twitter.com/alexafernands/"
+				className="fa fa-twitter"
+			></a>
+			<a
+				href="https://www.linkedin.com/in/alex-fernandes-5850b535/"
+				className="fa fa-linkedin"
+			></a>
+			<a
+				href="https://www.instagram.com/aafernands/"
+				className="fa fa-instagram"
+			></a>
+			<a href="https://github.com/aafernands/" className="fa fa-github"></a>
+			<p>
+				<span className="credits-footer">
+					© 2021 Website Designed by{" "}
+					<a href="https://twitter.com/alexafernands">AlexFernandes</a>
+				</span>
+			</p>
+		</footer>
 	);
 }
 

cffda090d60a8c30ae958a4c50d01718f69f9af5
Alex Fernandes <alexfernands@outlook.com>
2021-01-30 16:33:31 -0500

    refactor home page

--- src/App.js
+++ src/App.js
@@ -3,28 +3,29 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import NavTabs from "./components/NavTabs/NavTabs";
 import Footer from "./components/Footer/Footer";
 
-
 import Home from "./pages/HomePage/Home";
 import About from "./pages/AboutPage/About";
 import Contact from "./pages/ContactPage/Contact.js";
 import Portifolio from "./pages/PortifolioPage/Portifolio.js";
+import "./shared/style/global.css";
+import { Container } from "@material-ui/core";
 
-
-
-function App() { 
+function App() {
 	return (
-		<Router>
-			<div>
-				<NavTabs />
-				<Route exact path="/" component={Home} />
-				<Route exact path="/about" component={About} />
-				<Route exact path="/contact" component={Contact} />
-				<Route path="/portifolio" component={Portifolio} />
-				<Route path="/footer" component={Footer} />
-			</div>
+		<Container>
+			<Router>
+				<div>
+					<NavTabs />
+					<Route exact path="/" component={Home} />
+					<Route exact path="/about" component={About} />
+					<Route exact path="/contact" component={Contact} />
+					<Route path="/portifolio" component={Portifolio} />
+					<Route path="/footer" component={Footer} />
+				</div>
 
-			<Footer />
-		</Router>
+				<Footer />
+			</Router>
+		</Container>
 	);
 }
 
--- src/pages/HomePage/Home.js
+++ src/pages/HomePage/Home.js
@@ -1,45 +1,28 @@
 import React from "react";
-import Logo from "./alex.png";
 import { Button } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Avatar, Typography } from "@material-ui/core";
-import { Grid, Paper } from "@material-ui/core";
+import { Grid, Box, Container } from "@material-ui/core";
 
 import StarIcon from "@material-ui/icons/Star";
 import "../HomePage/HomePage.css";
-import Typical from "react-typical";
+import InfoCard from "./components/infoCard/infoCard";
 import Particles from "react-particles-js";
-import zIndex from "@material-ui/core/styles/zIndex";
 
 const userStyles = makeStyles((theme) => ({
-	avatar: {
-		position: "absolute",
-		bottom: "50%",
-		width: theme.spacing(30),
-		height: theme.spacing(30),
-		margin: theme.spacing(1),
-		zIndex: 1,
-	},
-	title: {
+	root: {
+		position: "relative",
 		color: "white",
-	},
-	typedContainer: {
-		position: "absolute",
-		top: "50%",
-		left: "50%",
-		transform: "translate(-50%, -50%)",
-		width: "100vw",
-		textAlign: "center",
-		zIndex: 1,
 	},
 	particles: {
 		position: "absolute",
-		top: "50%",
-		left: "50%",
-		transform: "translate(-50%, -50%)",
-		width: "100vw",
-		textAlign: "center",
-		zIndex: -4,
+		top: 0,
+		left: 0,
+		width: "100%",
+		height: "100%",
+		// height: 620,
+	},
+	paragraph: {
+		width: 500,
 	},
 }));
 
@@ -47,54 +30,53 @@ function Home() {
 	const classes = userStyles();
 
 	return (
-		<Grid container spacing={2} className={classes.grid}>
-			<Grid item xs={12}>
+		<div className={classes.root}>
+			<Particles
+				className={classes.particles}
+				params={{
+					particles: {
+						number: {
+							value: 70,
+						},
+					},
+				}}
+			>
+				<h1>testing</h1>
+			</Particles>
+			<Box alignItems="center" justifyContent="center">
 				<div className="home">
-					<Box className={classes.typedContainer}>
-						<Grid container justify="center">
-							<Avatar className={classes.avatar} src={Logo} alt="Alex" />
+					<Grid
+						container
+						direction="column"
+						justify="center"
+						alignItems="center"
+					>
+						<Grid>
+							<InfoCard />
 						</Grid>
-
-						<container maxWidth="sm">
-							<Typography className={classes.title} variant="h4">
-								<Typical
-									steps={[
-										"I am alex fernandes",
-										1500,
-										"I am a web developer!",
-										2300,
-									]}
-									loop={Infinity}
-									wrapper="p"
-								/>
-							</Typography>
-							<Particles
-								className={classes.particles}
-								params={{
-									particles: {
-										number: {
-											value: 40,
-										},
-									},
-								}}
-							/>
-						</container>
-					</Box>
-					<Grid className="">
-						<Button
-							startIcon={<StarIcon />}
-							size="large"
-							style={{ fontSize: 12, padding: 18 }}
-							variant="contained"
-							color="secondary"
-							href="./about"
-						>
-							MORE ABOUT ME
-						</Button>
+						<Grid>
+							<p className={classes.paragraph}>
+								I'm a Web Developer student based in New Jersey, United States.
+								I strive to build immersive and beautiful web applications
+								through carefully crafted code and user-centric design.
+							</p>
+						</Grid>
+						<Grid>
+							<Button
+								startIcon={<StarIcon />}
+								size="large"
+								style={{ fontSize: 12, padding: 18 }}
+								variant="contained"
+								color="secondary"
+								href="/about"
+							>
+								MORE ABOUT ME
+							</Button>
+						</Grid>
 					</Grid>
 				</div>
-			</Grid>
-		</Grid>
+			</Box>
+		</div>
 	);
 }
 

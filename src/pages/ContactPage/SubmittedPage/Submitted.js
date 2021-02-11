f8d9467fe0991bc502787b6a75fc6b9a71025646
Alex Fernandes <62030201+aafernands@users.noreply.github.com>
2021-02-10 23:42:12 -0500

    fixed contact page and minor bugs

--- src/pages/ContactPage/SubmittedPage/Submitted.js
+++ src/pages/ContactPage/SubmittedPage/Submitted.js
@@ -2,18 +2,76 @@ import React from "react";
 import TextField from "@material-ui/core/TextField";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
+import Particles from "react-particles-js";
 
-const useStyles = makeStyles({});
-
+const userStyles = makeStyles((theme) => ({
+	root: {
+		position: "relative",
+		color: "white",
+		background: "#000000ea;",
+		height: "76vh",
+	},
+	particles: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		width: "100%",
+		height: "100%",
+		// height: 620,
+	},
+	message: {
+		display: "flex",
+		alignItems: "center",
+		justifyContent: "center",
+	},
+}));
 export default function Submitted() {
-	const classes = useStyles();
+	const classes = userStyles();
 
 	return (
-		<Container className="container">
-					<h1 className="display-4">
-						THANK YOU FOR CONTACTING ME! <span id="spanTitle">ME </span>{" "}
-					</h1>
-					<p className="lead"> I WILL GET BACK TO YOU AS SOON AS POSSIBLE </p>
+		<Container className={classes.root}>
+			<Particles
+				className={classes.particles}
+				params={{
+					particles: {
+						number: {
+							value: 70,
+						},
+					},
+				}}
+			>
+				<h1>testing</h1>
+			</Particles>
+			<h1
+				style={{
+					display: "flex",
+					textAlign: "center",
+					justifyContent: "center",
+					position: "absolute",
+					left: "50%",
+					top: "50%",
+					transform: "translate(-50%, -50%)",
+				}}
+			>
+				THANK YOU FOR CONTACTING ME!
+			</h1>{" "}
+			<span>
+				<p
+					style={{
+						display: "flex",
+						alignItems: "center",
+						justifyContent: "center",
+						position: "absolute",
+						left: "50%",
+						top: "65%",
+						transform: "translate(-50%, -50%)",
+					}}
+					className="lead"
+				>
+					{" "}
+					I WILL GET BACK TO YOU AS SOON AS POSSIBLE{" "}
+				</p>
+			</span>
 		</Container>
 	);
 }
